690e3724750c7d2972b570ff7237beb90012a49b
nateglims <nglimsdale@gmail.com>
2019-09-15 10:01:43 -0700

    Use readlink to fix linking issues

--- setup.sh
+++ setup.sh
@@ -1,25 +1,24 @@
 # Vim Setup
-if [ ! -e ~/.vimrc ]; then
+if [ ! -e ~/.vimrc ] ; then
     echo "Linking vimrc..."
-    ln -s ./vimrc ~/.vimrc
+    ln -s $(readlink -f ./vimrc) ~/.vimrc
 fi
 
-if [ ! -d ~/.vim/bundle ]; then
+if [ ! -d ~/.vim/bundle ] ; then
     echo "Installing vundle..."
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
     vim +PluginInstall +qall
 fi
 
 # Emacs Setup
-if [ ! -d ~/.emacs.d ]; then
+if [ ! -d ~/.emacs.d ] ; then
     echo "Linking emacs.d..."
-    ln -s ./emacs.d ~/.emacs.d
+    ln -s $(readlink -f ./emacs.d) ~/.emacs.d
 fi
 
 # Tmux Setup
-if [ ! -e ~/.tmux.conf]; then
+if [ ! -e ~/.tmux.conf ] ; then
     echo "Linking TMUX Config..."
-    ln -s ./tmux.conf ~/.tmux.conf
+    ln -s $(readlink -f ./tmux.conf) ~/.tmux.conf
 fi
 
-# The thing about the bash rc is that it's really environment dependent...
