677396e7ac671f7d7b0ad9e8064af08eeac2c757
nateglims <nglimsdale@gmail.com>
2022-10-28 12:28:21 -0700

    neovim: load friendly snippet snippets

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -3,6 +3,8 @@ local cmp = require('cmp')
 local rust_tools = require('rust-tools')
 local luasnip = require('luasnip')
 
+require("luasnip/loaders/from_vscode").lazy_load()
+
 local has_words_before = function()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
@@ -45,7 +47,7 @@ cmp.setup({
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
-    { name = 'luasnip', option = { show_autosnippets = true } }, -- For vsnip users.
+    { name = 'luasnip', option = { show_autosnippets = true } },
   }, {
     { name = 'buffer' },
   })
