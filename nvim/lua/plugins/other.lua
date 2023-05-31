4ae86cdeb895d224648ca2c0954a6efffdeea14e
nateglims <nglimsdale@gmail.com>
2023-05-31 14:20:30 -0700

    neovim: add which-key plugin

--- nvim/lua/plugins/other.lua
+++ nvim/lua/plugins/other.lua
@@ -11,4 +11,17 @@ return {
     'simrat39/rust-tools.nvim',
     ft = "rust",
   },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  }
 }
