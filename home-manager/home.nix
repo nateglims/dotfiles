6456fc89c842923d44d9c2906594d7372909f910
nateglims <nglimsdale@gmail.com>
2023-08-20 20:59:22 -0700

    nixos: set up tmux

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -20,7 +20,6 @@
   home.packages = with pkgs; [
     _1password-gui
     discord
-    tmux
     exa
     ripgrep
     fd
@@ -73,4 +72,25 @@
     ./neovim.nix
     ./fish.nix
   ];
+
+  programs.tmux = {
+    enable = true;
+    clock24 = true;
+    plugins = with pkgs.tmuxPlugins; [
+      sensible
+      yank
+      {
+        plugin = dracula;
+        extraConfig = ''
+          set -g @dracula-show-battery false
+          set -g @dracula-show-powerline true
+          set -g @dracula-refresh-rate 10
+        '';
+      }
+    ];
+
+    extraConfig = ''
+      set -g mouse on
+    '';
+  };
 }
