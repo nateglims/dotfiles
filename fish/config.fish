24178688bc1b49e75ba68fa564f3016af358ad3c
nateglims <nglimsdale@gmail.com>
2023-09-05 14:04:50 -0700

    fish: update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,15 +1,16 @@
-#function fish_prompt
-#    set_color purple
-#    date "+%m/%d/%y"
-#    set_color F00
-#    echo (pwd) '>' (set_color normal)
-#end
-
-# status is-login; and pyenv init --path | source
-# status is-interactive; and pyenv init - | source
+fish_config theme choose "Dracula Official"
 
 if command -v exa > /dev/null
     alias ls="exa"
 end
 
-starship init fish | source
+if command -v starship > /dev/null
+    starship init fish | source
+else
+    function fish_prompt
+        set_color purple
+        date "+%m/%d/%y"
+        set_color F00
+        echo (pwd) '>' (set_color normal)
+    end
+end
