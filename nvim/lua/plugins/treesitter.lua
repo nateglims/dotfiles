cf11e4e055232cd7d2642b4e2a53a1312020e830
Glimsdale, Nathan <glimsdal@amazon.com>
2024-01-27 14:27:51 -0800

    neovim: update treesitter grammars

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
       local parser_config = require 'nvim-treesitter.parsers'.get_parser_configs()
       -- Tree Sitter setup
       require 'nvim-treesitter.configs'.setup {
-        ensure_installed = { "rust", "go", "typescript", "c", "lua", "bitbake" },
+        ensure_installed = { "rust", "go", "gomod", "yaml", "python", "typescript", "c", "lua", "bitbake" },
         ignore_install = {},
         highlight = {
           enable = true,
