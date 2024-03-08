70d742854fcd100ca5dff953800be1ae983641d4
Glimsdale, Nathan <glimsdal@amazon.com>
2024-03-08 11:36:26 -0800

    neovim: move neorg config.

--- nvim/lua/plugins/other.lua
+++ nvim/lua/plugins/other.lua
@@ -31,39 +31,4 @@ return {
       -- refer to the configuration section below
     }
   },
-  { "folke/zen-mode.nvim" },
-  {
-    "nvim-neorg/neorg",
-    ft = 'norg',
-    cmd = 'Neorg',
-    build = ":Neorg sync-parser",
-    keys = {
-      { '<Leader>cc', '<cmd>Neorg toggle-concealer<CR>', desc = "Toggle concealer" },
-    },
-    config = function()
-      require('neorg').setup {
-        load = {
-          ['core.defaults'] = {},
-          ['core.concealer'] = {},
-          ['core.integrations.treesitter'] = {},
-          ['core.export'] = {},
-          ['core.presenter'] = {
-            config = {
-              zen_mode = "zen-mode",
-            },
-          },
-          ['core.export.markdown'] = {
-            config = { extensions = "all" },
-          },
-          ['core.dirman'] = {
-            config = {
-              workspaces = {
-                notes = "~/WorkDocs/notes",
-              },
-            },
-          },
-        },
-      }
-    end,
-  },
 }
