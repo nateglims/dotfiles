e8a0c5d72fd85f234f080c235e37a0cf5153e972
nateglims <nglimsdale@gmail.com>
2024-03-09 11:18:50 -0800

    neovim: add telescope ui select for code actions.

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,6 +4,7 @@ return {
     lazy = false,
     dependencies = {
       'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope-ui-select.nvim',
     },
     config = function()
       local opts = { noremap = true, silent = true }
@@ -12,8 +13,20 @@ return {
       -- Keybindings
       set_keymap('n', '<Leader>g', ':Telescope live_grep<CR>', opts)
       set_keymap('n', '<Leader>s', ':Telescope lsp_document_symbols<CR>', opts)
+      set_keymap('n', '<Leader>S', ':Telescope lsp_workspace_symbols<CR>', opts)
       set_keymap('n', '<Leader>n', ':Telescope find_files previewer=false<CR>', opts)
       set_keymap('n', '<Leader>b', ':Telescope buffers<CR>', opts)
+
+      local telescope = require('telescope')
+      telescope.setup {
+        extensions = {
+          ["ui-select"] = {
+            require('telescope.themes').get_dropdown {}
+          }
+        }
+      }
+
+      telescope.load_extension("ui-select")
     end
   },
 }
