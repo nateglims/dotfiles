a853e4e9b8c808b04e6d2d0cc789d3ce9b4eee30
nateglims <nglimsdale@gmail.com>
2024-10-19 19:14:17 -0700

    wezterm: create tmux-like bindings

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,6 +1,14 @@
 local wezterm = require 'wezterm'
 local config = {}
 
+wezterm.on('update-right-status', function(window, pane)
+  local name = window:active_key_table()
+  if name then
+    name = 'TABLE: ' .. name
+  end
+  window:set_right_status(name or '')
+end)
+
 config.color_scheme = 'Tokyo Night'
 
 config.font = wezterm.font('FiraCode Nerd Font Mono')
@@ -10,5 +18,45 @@ if wezterm.target_triple == 'x86_64-unknown-linux-gnu' then
 else
   config.font_size = 20
 end
+
+-- config.leader = { key = 'a', mods = 'CTRL', timeout_millisecond = 1000 }
+config.keys = {
+  {
+    key = 'a',
+    mods = 'CTRL',
+    action = wezterm.action.ActivateKeyTable { name = 'tmux_like', timeout_millisecond = 1000 },
+  },
+}
+
+config.key_tables = {
+  tmux_like = {
+    {
+      key = '"',
+      mods = 'SHIFT',
+      action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+    },
+    {
+      key = '%',
+      mods = 'SHIFT',
+      action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+    },
+    {
+      key = 'c',
+      action = wezterm.action.SpawnTab('CurrentPaneDomain'),
+    },
+    {
+      key = 'n',
+      action = wezterm.action.ActivateTabRelative(1),
+    },
+    {
+      key = 'p',
+      action = wezterm.action.ActivateTabRelative(-1),
+    },
+    {
+      key = 'x',
+      action = wezterm.action.CloseCurrentTab { confirm = true },
+    },
+  },
+}
 
 return config
