e462cc69d440ba912d7fc0dd47d5b62144a54d33
nateglims <nglimsdale@gmail.com>
2024-10-23 17:30:13 -0700

    neovim: disable neorg
    There are issues with lazy loading and installling plugins. I also am
    not using this a lot right now...

--- nvim/lua/plugins/neorg.lua
+++ nvim/lua/plugins/neorg.lua
@@ -1,39 +1,40 @@
-return {
-  -- Zen mode is needed for neorg presentations.
-  { "folke/zen-mode.nvim" },
-  {
-    "nvim-neorg/neorg",
-    ft = 'norg',
-    lazy = true,
-    cmd = 'Neorg',
-    build = ":Neorg sync-parser",
-    keys = {
-      { '<Leader>cc', '<cmd>Neorg toggle-concealer<CR>', desc = "Toggle concealer" },
-    },
-    config = function()
-      require('neorg').setup {
-        load = {
-          ['core.defaults'] = {},
-          ['core.concealer'] = {},
-          ['core.integrations.treesitter'] = {},
-          ['core.export'] = {},
-          ['core.presenter'] = {
-            config = {
-              zen_mode = "zen-mode",
-            },
-          },
-          ['core.export.markdown'] = {
-            config = { extensions = "all" },
-          },
-          ['core.dirman'] = {
-            config = {
-              workspaces = {
-                notes = "~/WorkDocs/notes",
-              },
-            },
-          },
-        },
-      }
-    end,
-  },
-}
+-- return {
+--   -- Zen mode is needed for neorg presentations.
+--   { "folke/zen-mode.nvim" },
+--   {
+--     "nvim-neorg/neorg",
+--     ft = 'norg',
+--     lazy = true,
+--     cmd = 'Neorg',
+--     build = ":Neorg sync-parser",
+--     keys = {
+--       { '<Leader>cc', '<cmd>Neorg toggle-concealer<CR>', desc = "Toggle concealer" },
+--     },
+--     config = function()
+--       require('neorg').setup {
+--         load = {
+--           ['core.defaults'] = {},
+--           ['core.concealer'] = {},
+--           ['core.integrations.treesitter'] = {},
+--           ['core.export'] = {},
+--           ['core.presenter'] = {
+--             config = {
+--               zen_mode = "zen-mode",
+--             },
+--           },
+--           ['core.export.markdown'] = {
+--             config = { extensions = "all" },
+--           },
+--           ['core.dirman'] = {
+--             config = {
+--               workspaces = {
+--                 notes = "~/WorkDocs/notes",
+--               },
+--             },
+--           },
+--         },
+--       }
+--     end,
+--   },
+-- }
+return {}
