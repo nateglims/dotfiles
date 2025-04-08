9eafd964b44740b42e6688e365ae8ec073303110
nateglims <nglimsdale@gmail.com>
2025-04-07 18:12:09 -0700

    neovim: fix pyright settings.

--- nvim/lsp/pyright.lua
+++ nvim/lsp/pyright.lua
@@ -1,5 +1,15 @@
 return {
-  cmd = { 'pyright' },
+  cmd = { 'pyright-langserver', '--stdio' },
   root_markers = { 'pyproject.toml', 'setup.cfg', '.git' },
   filetypes = { 'python' },
+  single_file_support = true,
+  settings = {
+    python = {
+      analysis = {
+        autoSearchPaths = true,
+        useLibraryCodeForTypes = true,
+        diagnosticMode = 'openFilesOnly',
+      },
+    },
+  },
 }
